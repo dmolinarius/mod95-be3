42ca80fd45961f665361cbb3c6dd53a6ced3dd07
dmolinarius <dmolinarius@gmail.com>
2018-10-16 10:05:44 +0200

    Refactor middlewares

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express')
   , static_pages = require('serve-static')
+  , body_parser = require('body-parser')
+  , urlencoded_parser = body_parser.urlencoded({extended: false})
   , fs = require('fs')
   , md5 = require('md5')
   , app = express()
@@ -14,43 +16,59 @@ var express = require('express')
 /*
 ** resources with content-type not matching file extension
 */
-app.use('/file1.html', function(request,response,next) {
-  send_fake(response,'htdocs/hello.jpeg','image/jpeg');
+app.get('/file1.html', function(request,response,next) {
+  request.params = { file: 'htdocs/hello.jpeg', type:'image/jpeg' };
+  send_fake(request,response);
 });
-app.use('/file2.html', function(request,response,next) {
-  send_fake(response,'htdocs/hello.gif','image/gif');
+app.get('/file2.html', function(request,response,next) {
+  request.params = { file: 'htdocs/hello.gif', type:'image/gif' };
+  send_fake(request,response);
 });
-app.use('/file3.html', function(request,response,next) {
-  send_fake(response,'htdocs/hello.pdf','application/pdf');
+app.get('/file3.html', function(request,response,next) {
+  request.params = { file: 'htdocs/hello.pdf', type:'application/pdf' };
+  send_fake(request,response);
 });
 
 /*
 ** redirecting resources
 */
-app.use('/redirect', function(request,response,next) {
+app.get('/redirect', function(request,response,next) {
   response.writeHead(301, { 'Location': 'http://www.ec-lyon.fr' });
   response.end();
 });
 
 /*
-** password protected resources
+** HTTP Basic protected resource
 */
-app.use('/user.html', function(request,response,next) {
+app.get('/user.html', function(request,response,next) {
   var auth = request.headers.authorization;
   if ( auth && auth.indexOf('Basic') === 0 ) {
     var encoded = auth.split(' ')[1].trim()
       , decoded = new Buffer(encoded,'base64').toString('ascii')
     ;
     if ( decoded == 'be-http:cool!' ) next();
-    else send_401_basic(response);
+    else send_401_basic(request,response);
   }
-  else send_401_basic(response);
+  else send_401_basic(request,response);
 });
-app.use('/401/basic', function(request,response,next) {
-  send_401_basic(response);
+app.get('/401/basic', send_401_basic);
+
+// encode
+app.get('/encode/*', function(request,response,next) {
+  var url = request.url.substring(1)
+    , encoded = Buffer.from(url).toString('base64')
+  ;
+  // console.log(url,encoded);
+  response.writeHead(200, {
+    'Content-Type': 'text/plain; charset=utf-8'
+  });
+  response.end(encoded);
 });
 
-app.use('/digest.html', function(request,response,next) {
+/*
+** HTTP Digest protected resource
+*/
+app.get('/digest.html', function(request,response,next) {
   var auth = request.headers.authorization;
   if ( auth ) {
     let info = auth_info(auth)
@@ -59,32 +77,54 @@ app.use('/digest.html', function(request,response,next) {
       , expected = md5(A1+':'+info.nonce+':'+A2)
     ;
     if ( info.response == expected ) next();
+    else send_401_digest(request,response);
   }
-  else send_401_digest(response);
+  else send_401_digest(request,response);
 });
-app.use('/401/digest', function(request,response,next) {
-  send_401_digest(response);
-});
+app.get('/401/digest/*', send_401_digest);
 
-/*
-** base64 encode
-*/
-app.use('/encode', function(request,response,next) {
-  var url = request.url.substring(1)
-    , encoded = Buffer.from(url).toString('base64')
-  ;
-  // console.log(url,encoded);
-  response.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-    });
-  response.end(encoded);
-});
-app.use('/md5', function(request, response, next) {
+// MD5
+app.get('/md5', function(request, response, next) {
   var url = request.url.substring(1);
   response.writeHead(200, {
     'Content-Type': 'text/plain; charset=utf-8'
-    });
+  });
   response.end(md5(url));
+});
+
+
+/* ****************************************************************************
+**
+** List service
+**
+** ***************************************************************************/
+var lists = {};
+
+/*
+** POST /list  - create a list
+**   params : name
+**   returns : empty list
+*/
+
+// GET list/$listid
+// PUT /list/$listid update
+// DELETE /list/$listid
+
+
+/*
+** 405 Method Not Allowed
+*/
+app.delete('*',function(request,response) {
+  send_405(request, response);
+});
+app.patch('*',function(request,response) {
+  send_405(request, response);
+});
+app.post('*',function(request,response) {
+  send_405(request, response);
+});
+app.put('*',function(request,response) {
+  send_405(request, response);
 });
 
 /*
@@ -96,64 +136,71 @@ app.use(static_pages('htdocs'));
 ** 404 Not Found
 */
 app.use(function(request,response) {
-  send_404(response);
+  send_404(request, response);
 });
 
 
-/* ****************************************************************************
-**
-** Manage lists
-**
-** ***************************************************************************/
-var lists = [];
-
-// GET list/$listid
-// PUT /list/$listid update
-// POST /list create
-// DELETE /list/$listid
 
 /* ****************************************************************************
 **
-** Send various responses
+** Local middleware
 **
 ** ***************************************************************************/
 
-function send_401_basic(response) {
+function send_401_basic(request,response) {
   response.writeHead(401, {
     'WWW-Authenticate': 'Basic realm="BE-HTTP"',
     'Content-Type': 'text/plain; charset=utf-8'
   });
   response.end('Désolé, ce document est protégé par mot de passe...');
 }
-function send_401_digest(response) {
+function send_401_digest(request,response) {
   response.writeHead(401, {
     'WWW-Authenticate': 'Digest realm="BE-HTTP", nonce="abc"',
     'Content-Type': 'text/plain; charset=utf-8'
   });
   response.end('Désolé, ce document est protégé par mot de passe...');
 }
-function send_404(response) {
+function send_404(request,response) {
+  console.log('Hello from 404 middleware');
   response.writeHead(404, {
     'Content-Type': 'text/plain; charset=utf-8'
     });
   response.end('Désolé, le document demandé est introuvable...');
 }
-function send_fake(response,file,type) {
-  var sent_header = false
-    , stream = fs.createReadStream(file)
+function send_405(request,response) {
+  console.log('Hello from 405 middleware');
+  response.writeHead(405, {
+    'Content-Type': 'text/plain; charset=utf-8'
+    });
+  response.end('Méthode non autorisée...');
+}
+
+function send_fake(request, response) {
+  var file = request.params.file
+    , type = request.params.type
   ;
-  stream.on('error', function(e) {
-    // console.log(e);
-    send_404(response);
-  });
-  stream.on('data', function(data) {
-    if ( ! sent_header ) {
-      response.writeHead(200, { 'Content-Type': type });
-      sent_header = true;
+  fs.stat(file, function(err, stats) {
+    if ( err ) {
+	console.log(err);
+        send_404(response);
+        return;
     }
-    response.write(data);
+    response.writeHead(200, {
+      'Content-Type': type,
+      'Content-Length': stats.size,
+      'Last-Modified': stats.mtime.toUTCString()
+    });
+    let stream = fs.createReadStream(file);
+    stream.on('error', function(e) { send_404(response); });
+    stream.on('data', function(data) { response.write(data); });
+    stream.on('end', function(data) { response.end(); });
   });
-  stream.on('end', function(data) { response.end(); });
+}
+
+function send_json(request, response) {
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.end(JSON.stringify(response.data || null));
 }
 
 
