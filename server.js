4ba1eeb3989e755b2af0f65e39770b5e4c3b0965
dmolinarius <dmolinarius@gmail.com>
2018-10-27 11:14:41 +0200

    refactor to use error rendering middleware to send status code

--- server.js
+++ server.js
@@ -30,57 +30,44 @@ app.set('views', VIEWS_PATH);
 /*
 ** resources with content-type not matching file extension
 */
-app.get('/file1.html', function(request,response,next) {
-  request.params = { file: 'htdocs/hello.jpeg', type:'image/jpeg' };
-  send_fake(request,response);
-});
-app.get('/file2.html', function(request,response,next) {
-  request.params = { file: 'htdocs/hello.gif', type:'image/gif' };
-  send_fake(request,response);
-});
-app.get('/file3.html', function(request,response,next) {
-  request.params = { file: 'htdocs/hello.pdf', type:'application/pdf' };
-  send_fake(request,response);
-});
+function fake(request, response, next) {
+  var fake_files = {
+    '/file1.html': { file: 'htdocs/hello.jpeg', type:'image/jpeg' },
+    '/file2.html': { file: 'htdocs/hello.gif',  type:'image/gif' },
+    '/file3.html': { file: 'htdocs/hello.pdf',  type:'application/pdf' }
+  };
+  request.params = fake_files[request.route.path];
+  next();
+}
+app.get('/file1.html', fake, send_fake);
+app.get('/file2.html', fake, send_fake);
+app.get('/file3.html', fake, send_fake);
+
 
 /*
 ** redirecting resources
 */
-app.get('/moved', function(request,response,next) {
-  response.writeHead(301, { 'Location': 'http://www.ec-lyon.fr' });
-  response.end();
-});
-app.get('/found', function(request,response,next) {
-  response.writeHead(302, { 'Location': 'http://www.ec-lyon.fr' });
-  response.end();
-});
-app.get('/temp', function(request,response,next) {
-  response.writeHead(307, { 'Location': 'http://www.ec-lyon.fr' });
-  response.end();
-});
-app.get('/perm', function(request,response,next) {
-  response.writeHead(308, { 'Location': 'http://www.ec-lyon.fr' });
-  response.end();
-});
+app.get('/moved', err(301)('http://www.ec-lyon.fr'));
+app.get('/found', err(302)('http://www.ec-lyon.fr'));
+app.get('/temp',  err(307)('http://www.ec-lyon.fr'));
+app.get('/perm',  err(308)('http://www.ec-lyon.fr'));
 
 /*
 ** HTTP Basic protected resources
 */
 function check_basic_user(user,password) {
   return (user == 'be-http' && password == 'cool!') ? {user, password} : null;
-};
-
+}
 realm = 'BE-HTTP';
 app.get('/user.html', basic_auth(realm, check_basic_user));
 app.get('/401/basic', err(401).basic(realm));
 
 // encode
-app.get('/encode/*', (req,res,next) => {
-    res.body = Buffer.from(req.params[0]).toString('base64');
-    next();
-  },
-  send_text
-);
+function encode(request, response, next) {
+  response.body = Buffer.from(request.params[0]).toString('base64');
+  next();
+}
+app.get('/encode/*', encode, send_text);
 
 /*
 ** HTTP Digest protected resources
@@ -98,15 +85,12 @@ realm = 'Digest 2';
 app.get('/digest2.html', digest_auth(realm, check_digest_user, create_nonce, check_nonce));
 app.get('/401/digest2', err(401).digest(realm,create_nonce));
 
-app.use(render_error);
-
 // MD5
-app.get('/md5/*', (req,res,next) => {
-    res.body = md5(req.params[0]);
-    next();
-  },
-  send_text
-);
+function encrypt(request, response, next) {
+  response.body = md5(request.params[0]);
+  next();
+}
+app.get('/md5/*', encrypt, send_text);
 
 
 /* ****************************************************************************
@@ -120,7 +104,7 @@ let lists = {}
   , new_list = (value) => {
       let name = list_name();
       if ( lists[name] ) return new_list();
-      lists[name] = { name, data:  list_data(value) };
+      lists[name] = { name, data: list_data(value) };
       return lists[name];
     }
 ;
@@ -130,6 +114,10 @@ let lists = {}
 **   param : value = String | [String]
 **   returns : new todolist
 */
+function create_list(request,response,next) {
+  response.data = new_list(request.body.value);
+  next();
+}
 app.post('/todolist', check_content_types([
     'application/x-www-form-urlencoded',
     'application/json'
@@ -137,10 +125,8 @@ app.post('/todolist', check_content_types([
   json_parser,
   urlencoded_parser,
   check_params('body',['value']),
-  function(request,response,next) {
-    response.data = new_list(request.body.value);
-    send_json(request,response);
-  }
+  create_list,
+  send_json
 );
 
 /*
@@ -148,6 +134,11 @@ app.post('/todolist', check_content_types([
 **   param : value = String | [String]
 **   returns : modified todolist
 */
+function replace_list(request,response,next) {
+  lists[request.params.id].data = list_data(request.body.value);
+  response.data = lists[request.params.id];
+  next();
+}
 app.put('/todolist/:id', check_content_types([
     'application/x-www-form-urlencoded',
     'application/json'
@@ -156,11 +147,8 @@ app.put('/todolist/:id', check_content_types([
   urlencoded_parser,
   check_params('body',['value']),
   check_list,
-  function(request,response,next) {
-    lists[request.params.id].data = list_data(request.body.value);
-    response.data = lists[request.params.id];
-    send_json(request,response);
-  }
+  replace_list,
+  send_json
 );
 
 /*
@@ -171,21 +159,27 @@ app.put('/todolist/:id', check_content_types([
 **   param : value = String | [String]
 **   returns : patched todolist
 */
+function splice_list(request,response,next) {
+  if ( request.body.value !== undefined ) {
+    lists[request.params.id].data.splice(
+      request.body.index, request.body.delete, ...list_data(request.body.value));
+  }
+  else { 
+    lists[request.params.id].data.splice(request.body.index, request.body.delete);
+  }
+  response.data = lists[request.params.id];
+  next();
+}
 app.patch('/todolist/:id', check_content_types([
     'application/x-www-form-urlencoded',
     'application/json'
   ]),
   json_parser,
   urlencoded_parser,
-  check_params('body',['index','delete','value']),
+  check_params('body',['index','delete']),
   check_list,
-  function(request,response,next) {
-    // TODO check index and delete to be integers in a reasonable range
-    lists[request.params.id].data.splice(
-      request.body.index, request.body.delete, ...list_data(request.body.value));
-    response.data = lists[request.params.id];
-    send_json(request,response);
-  }
+  splice_list,
+  send_json
 );
 
 /*
@@ -193,10 +187,8 @@ app.patch('/todolist/:id', check_content_types([
 */
 app.get('/todolist/:id',
   check_list,
-  function(request,response,next) {
-    response.data = lists[request.params.id];
-    send_json(request,response);
-  }
+  (req,res,next) => {res.data = lists[req.params.id]; next();},
+  send_json
 );
 
 /*
@@ -204,28 +196,18 @@ app.get('/todolist/:id',
 */
 app.delete('/todolist/:id',
   check_list,
-  function(request,response,next) {
-    delete lists[request.params.id];
-    response.status(204).end();
-  }
+  (req,res,next) => { delete lists[req.params.id]; next(); },
+  err(204)
 );
 
 
 /*
 ** 405 Method Not Allowed
 */
-app.delete('*',function(request,response) {
-  send_405(request, response);
-});
-app.patch('*',function(request,response) {
-  send_405(request, response);
-});
-app.post('*',function(request,response) {
-  send_405(request, response);
-});
-app.put('*',function(request,response) {
-  send_405(request, response);
-});
+app.delete('*',err(405));
+app.patch('*',err(405));
+app.post('*',err(405));
+app.put('*',err(405));
 
 /*
 ** static pages
@@ -235,9 +217,12 @@ app.use(static_pages('htdocs'));
 /*
 ** 404 Not Found
 */
-app.use(function(request,response) {
-  send_404(request, response);
-});
+app.use(err(404));
+
+/*
+** render errors
+*/
+app.use(render_error);
 
 
 
@@ -249,35 +234,29 @@ app.use(function(request,response) {
 
 function check_list(request,response,next) {
   var name = request.params.id;
-  if ( !lists[name] ) {
-    response.status(404).render( 'error.html', {
-      bgcolor: "#006",
-      code: "404 : Not Found",
-      msg:"La liste demandée n'existe pas"
-    });
-  }
-  else next();
+  if ( lists[name] ) next();
+  else err(404,"La liste demandée n'existe pas")(request,response,next);
 }
 function check_content_types(media_types) {
   return function(request,response,next) {
     var ct = request.get('Content-Type');
-    // 415 - Unsupported Media type
-    if ( media_types.indexOf(ct) == -1 ) {
-      response.detail = media_types;
-      send_415(request,response);
+    if ( media_types.indexOf(ct) > -1 ) next();
+    else { // 415 - Unsupported Media Type
+      message = "Type de contenu non supporté ('" + media_types.join("' ou '")+"' attendu)";
+      err(415, message)(request, response, next);
     }
-    else next();
   }
 }
 function check_params(ctx,params) {
   return function(request,response,next) {
     var missing = params.reduce((a,p) => request[ctx] && typeof request[ctx][p] != 'undefined' ? a : a.concat([p]),[]);
-    // 422 - Unprocessable entity
-    if ( missing.length ) {
-      response.detail = missing;
-      send_422(request,response);
+    if ( ! missing.length ) next();
+    else { // 422 - Unprocessable entity
+      plural = (missing.length > 1 ? 's':'');
+      message = "Impossible de traiter la requête" +
+        ", paramètre"+plural+" manquant" + plural + ' : ' + missing.join(', ');
+      err(422, message)(request, response, next);
     }
-    else next();
   }
 }
 
@@ -536,79 +515,39 @@ function get_client_ip(request) {
 
 /* ****************************************************************************
 **
-** Middleware to send default status messages
-**
-** ***************************************************************************/
-function send_404(request,response) {
-  response.status(404).render( 'error.html', {
-    bgcolor: "#006",
-    code: "404 : Not Found",
-    msg:"Désolé le document demandé est introuvable"
-  });
-}
-function send_405(request,response) {
-  response.status(404).render( 'error.html', {
-    bgcolor: "#600",
-    code: "405 : Method Not Allowed",
-    msg:"La méthode "+request.method+" n'est pas autorisée pour cette ressource"
-  });
-}
-function send_415(request,response) {
-  response.status(422).render( 'error.html', {
-    bgcolor: '#fa4',
-    code: "415 : Unsupported Media Type",
-    msg:"Type de contenu non supporté"+(response.detail ? " ('"+
-      response.detail.join("' ou '")+"' attendu)" : '')
-  });
-}
-function send_422(request,response) {
-  response.status(422).render( 'error.html', {
-    bgcolor: "#ec0",
-    code: "422 : Unprocessable Entity",
-    msg: "Impossible de traiter la requête"+(response.detail ?
-      ", paramètre"+(response.detail.length > 1 ? 's':'') +
-      " manquant"+(response.detail.length > 1 ? 's':'') + ' : ' +
-      response.detail.join(', ') : '')
-  });
-}
-
-
-/* ****************************************************************************
-**
 ** Middleware to send body with various content-types
 **
 ** ***************************************************************************/
-function send_fake(request, response) {
+function send_fake(request, response, next) {
   var file = request.params.file
     , type = request.params.type
   ;
   fs.stat(file, function(error, stats) {
     if ( error ) {
-	console.log(error);
-        send_404(response);
+        err(404)(request, response, next);
         return;
     }
-    response.writeHead(200, {
+    response.status(200).set({
       'Content-Type': type,
       'Content-Length': stats.size,
       'Last-Modified': stats.mtime.toUTCString()
     });
-    let stream = fs.createReadStream(file);
-    stream.on('error', function(e) { send_404(response); });
-    stream.on('data', function(data) { response.write(data); });
+    let sending = false
+      , stream = fs.createReadStream(file);
+    ;
+    stream.on('error', function(e) { sending ? response.end() : err(500)(request,response,next); });
+    stream.on('data', function(data) { sending = true; response.write(data); });
     stream.on('end', function(data) { response.end(); });
   });
 }
 
 function send_json(request, response) {
-  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.status(200).set({ 'Content-Type': 'application/json' });
   response.end(JSON.stringify(response.data || null));
 }
 
 function send_text(request, response) {
-  response.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
+  response.status(200).set({ 'Content-Type': 'text/plain; charset=utf-8' });
   response.end(response.body);
 }
 
@@ -636,8 +575,17 @@ function send_text(request, response) {
 function err(code, message=null) {
   var error = { code, message };
 
+  // 30x codes need location header
+  if ( (''+code).substring(0,2) == '30' ) {
+    return function(location) {
+      return function(request, response, next) {
+        response.status(code).set({ Location: location });
+        next({...error, location});
+      };
+    };
+  }
   // 401 needs authentication header
-  if ( code == 401 ) {
+  else if ( code == 401 ) {
     return {
 
       // basic authentication
@@ -676,17 +624,33 @@ function err(code, message=null) {
 **
 ** ***************************************************************************/
 
+error_300_data = {
+    bgcolor: '#aaa',
+    default_message: "Allez voir ailleurs..."
+}
 error_401_data = {
     bgcolor: '#060',
     code: "401 - Authorization Required"
 }
 error_data = {
+  '301': { ...error_300_data,
+    code: "301 - Moved Permanently"
+  },
+  '302': { ...error_300_data,
+    code: "302 - Found (Moved Temporarily)"
+  },
+  '307': { ...error_300_data,
+    code: "307 - Temporary Redirect"
+  },
+  '308': { ...error_300_data,
+    code: "308 - permanent Redirect"
+  },
   "401_basic": { ...error_401_data,
     bgcolor: '#066',
-    default_message: "Ce document est protégé par mot de passe"
+    default_message: "Document protégé par mot de passe"
   },
   "401_digest": { ...error_401_data,
-    default_message: "Ce document est protégé par mot de passe"
+    default_message: "Document protégé par mot de passe"
   },
   "unknown user": { ...error_401_data,
     default_message: "Utilisateur inconnu"
@@ -709,15 +673,35 @@ error_data = {
   'Stale nonce' : { ...error_401_data,
     default_message: "Durée de validité du nonce dépassée"
   },
-  'no auth_info': {
-    bgcolor: '#c00',
-    code: "500 - Internal server error",
-    default_message: "Dysfonctionnement inattendu, il manque auth_info"
+  '404' : {
+    bgcolor: "#006",
+    code: "404 - Not Found",
+    default_message:"Document introuvable"
+  },
+  '405' : {
+    bgcolor: "#600",
+    code: "405 - Method Not Allowed",
+    default_message: "Méthode interdite"
+  },
+  '415' : {
+    bgcolor: '#fa4',
+    code: "415 : Unsupported Media Type",
+    default_message: "Contenu non supporté"
+  },
+  '422': {
+    bgcolor: "#ec0",
+    code: "422 : Unprocessable Entity",
+    default_message: "Entité intraitable"
   },
   '500': {
     bgcolor: '#c00',
     code: "500 - Internal server error",
     default_message: "Dysfonctionnement serveur"
+  },
+  'no auth_info': {
+    bgcolor: '#c00',
+    code: "500 - Internal server error",
+    default_message: "Dysfonctionnement inattendu, auth_info manquant"
   },
   'default': {
     bgcolor: '#aaa',
@@ -729,19 +713,30 @@ function render_error(error, request, response, next) {
   var render = (o) => response.render('error.html', o);
   console.log('hello from render_error',error);
   if ( ! error ) next();
-  if ( error.code == 401 ) {
+  if ( (''+error.code).substring(0,2) == '30' ) {
+    render({...error_data[error.code],
+      message: 'Il faut aller voir <a href="'+error.location+'">'+error.location+'</a>'});
+  }
+  else if ( error.code == 401 ) {
     if ( error.message == 'unknown user') render({...error_data[error.message],
          message: "L'utilisateur "+request.auth_info.username+" est inconnu de nos services" });
     else if ( !error.nonce ) render(error_data['401_basic']);
     else if ( error.message in error_data ) render(error_data[error.message]);
     else render({...error_data['401_digest'], message:error.message});
   }
+  else if ( error.code == 405 ) {
+    render({...error_data['405'],
+      message:"La méthode "+request.method+" n'est pas autorisée pour cette ressource"});
+  }
   else if ( error.code == 500 ) {
     if ( error.message in error_data ) render(error_data[error.message]);
-    else render({...error_data['500'], ...error});
+    else render({...error, ...error_data['500']});
+  }
+  else if ( ''+error.code in error_data ) {
+    render({...error, ...error_data[''+error.code]});
   }
   else if ( error.code ) {
-    render({...error_data['default'], ...error});
+    render({...error, ...error_data['default']});
   }
   else next(error);
 }
